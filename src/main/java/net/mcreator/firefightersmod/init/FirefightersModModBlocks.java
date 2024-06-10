89bca6bdde744e577d62aa8f73bca91ac0cce1fd
jan <jan@pc-jan>
2024-06-10 19:11:11 +0200

    MCreator Merge commit 'dcda06543dda697b5d65004b9cbdf754b2126274' into 1f44a5352463fe5bb14c3f6cdfcbc78a64055586

--- src/main/java/net/mcreator/firefightersmod/init/FirefightersModModBlocks.java
+++ src/main/java/net/mcreator/firefightersmod/init/FirefightersModModBlocks.java
@@ -14,7 +14,6 @@ import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.level.block.Block;
 
-import net.mcreator.firefightersmod.block.UnifireBlock;
 import net.mcreator.firefightersmod.block.TiresBlock;
 import net.mcreator.firefightersmod.block.SmokeBlock;
 import net.mcreator.firefightersmod.block.PBtankBlock;
@@ -24,12 +23,11 @@ import net.mcreator.firefightersmod.FirefightersModMod;
 
 public class FirefightersModModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, FirefightersModMod.MODID);
-	public static final RegistryObject<Block> UNIFIRE = REGISTRY.register("unifire", () -> new UnifireBlock());
 	public static final RegistryObject<Block> P_BTANK = REGISTRY.register("p_btank", () -> new PBtankBlock());
 	public static final RegistryObject<Block> SMOKE = REGISTRY.register("smoke", () -> new SmokeBlock());
+	public static final RegistryObject<Block> TIRES = REGISTRY.register("tires", () -> new TiresBlock());
 	public static final RegistryObject<Block> CARDBOARDBOX = REGISTRY.register("cardboardbox", () -> new CardboardboxBlock());
 	public static final RegistryObject<Block> EMBERSBLOCK = REGISTRY.register("embersblock", () -> new EmbersblockBlock());
-	public static final RegistryObject<Block> TIRES = REGISTRY.register("tires", () -> new TiresBlock());
 
 	// Start of user code block custom blocks
 	// End of user code block custom blocks
--- src/main/java/net/mcreator/firefightersmod/init/FirefightersModModTabs.java
+++ src/main/java/net/mcreator/firefightersmod/init/FirefightersModModTabs.java
@@ -6,19 +6,14 @@ package net.mcreator.firefightersmod.init;
 
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import net.mcreator.firefightersmod.FirefightersModMod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class FirefightersModModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FirefightersModMod.MODID);
 	public static final RegistryObject<CreativeModeTab> FIREFIGHTERS_MOD = REGISTRY.register("firefighters_mod",
@@ -37,11 +32,4 @@ public class FirefightersModModTabs {
 				tabData.accept(FirefightersModModBlocks.CARDBOARDBOX.get().asItem());
 				tabData.accept(FirefightersModModBlocks.EMBERSBLOCK.get().asItem());
 			}).withSearchBar().build());
-
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-		if (tabData.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-			tabData.accept(FirefightersModModBlocks.UNIFIRE.get().asItem());
-		}
-	}
 }
